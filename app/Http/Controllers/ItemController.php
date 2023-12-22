93b90f025348b9fa5ef0d54bd590f6950066ce22
HoudaTeyeb <teyebhouda1994@gmail.com>
2023-12-22 23:10:30 +0100

    category

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -218,7 +218,7 @@ $productcategory=$category->id;
                        $matchingProduct->price = $apiPrice;
                    }
                    foreach ($categories as $category) {
-                    if($category->cat_name =="DESSERTS")
+                    if($apiFamille =="DESSERTS")
                   { dd($apiFamille);}
                     if ($category->cat_name == $apiFamille) {
                       //  dd($apiFamille);
