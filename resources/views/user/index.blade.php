c3d135eca8a1b5364e25c079263c5767494e060c
teyebhouda <teyebhouda1994@gmail.com>
2023-12-23 13:56:56 +0100

    translate

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -581,7 +581,7 @@
                      <ul class="textdata">
                         <li><a href="{{url('/')}}">{{__('messages.home')}}</a></li>
                         <li><a href="{{url('aboutus')}}">{{__('messages.aboutus')}}</a></li>
-                        <li><a href="{{url('service')}}">{{__('messages.service')}}</a></li>
+                        {{-- <li><a href="{{url('service')}}">{{__('messages.service')}}</a></li> --}}
                         <li><a href="{{url('contactus')}}">{{__('messages.contact')}}</a></li>
                      </ul>
                   </div>
