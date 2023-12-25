fa58725c0d279306dc3c2fc1287edcba9fac161d
teyebhouda <teyebhouda1994@gmail.com>
2023-12-25 13:11:13 +0100

    ingredients

--- public/js/code.js
+++ public/js/code.js
@@ -762,7 +762,7 @@ function orderplace() {
             .then(response => response.text())
             .then(checkdata => {
                 console.log(checkdata);
-                if (checkdata.trim() === '') {
+                if (checkdata === "[]") { 
                     console.log(phone);
                    
                             var newUserData = {
