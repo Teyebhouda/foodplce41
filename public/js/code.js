f1aa9cebefddfa1eae3c52784733517e53fa7492
HoudaTeyeb <teyebhouda1994@gmail.com>
2023-12-28 23:22:10 +0100

    retouches

--- public/js/code.js
+++ public/js/code.js
@@ -901,37 +901,37 @@ function addprice(price, iqty) {
         console.log(menu_new_price);
     }
    
+    $.each($("input[type='radio']:checked"), function () {
+        var previouslyCheckedPrice = 0; // Initialize previously checked price
+        
+        var price = $(this).data('price');
+        console.log(price);
+        var origin_price = parseFloat($("#origin_price").val());
+        var currentQuantity = parseFloat($('#number').val());
+        if (previouslyCheckedPrice !== 0) {
+            // Subtract the previously checked price
+            origin_price -= previouslyCheckedPrice;
+        }
     
+        var isChecked = $(this).prop("checked");
+    
+       
+            console.log("checked");
+            var parsedPrice = parseFloat(price); // Ensure price is parsed as a float
+            var menu_new_price = origin_price + parsedPrice;
+            var roundedPrice = Math.round(menu_new_price * 100) / 100; // Round to two decimal places
+            $("#origin_price").val(roundedPrice.toFixed(2));
+    
+            var pricedata = roundedPrice * currentQuantity;
+            document.getElementById("price").innerHTML = pricedata.toFixed(2);
+            console.log(roundedPrice.toFixed(2));
+            previouslyCheckedPrice = parsedPrice; 
+    });  
 
 }
 
 
-$.each($("input[type='radio']:checked"), function () {
-    var previouslyCheckedPrice = 0; // Initialize previously checked price
-    
-    var price = $(this).data('price');
-    console.log(price);
-    var origin_price = parseFloat($("#origin_price").val());
-    var currentQuantity = parseFloat($('#number').val());
-    if (previouslyCheckedPrice !== 0) {
-        // Subtract the previously checked price
-        origin_price -= previouslyCheckedPrice;
-    }
 
-    var isChecked = $(this).prop("checked");
-
-   
-        console.log("checked");
-        var parsedPrice = parseFloat(price); // Ensure price is parsed as a float
-        var menu_new_price = origin_price + parsedPrice;
-        var roundedPrice = Math.round(menu_new_price * 100) / 100; // Round to two decimal places
-        $("#origin_price").val(roundedPrice.toFixed(2));
-
-        var pricedata = roundedPrice * currentQuantity;
-        document.getElementById("price").innerHTML = pricedata.toFixed(2);
-        console.log(roundedPrice.toFixed(2));
-        previouslyCheckedPrice = parsedPrice; 
-});
 
 
 
